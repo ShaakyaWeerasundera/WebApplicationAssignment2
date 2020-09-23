d7dd09d8f444b45710cf3a99ddca0455639af996
Shaakya Weerasundera <s307943@students.cdu.edu.au>
2020-09-23 17:59:58 +0930

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationAssignment2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
